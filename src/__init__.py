63dc000025465060543fb1404ed70a32ff729a40
Bo Yuan <boyuxa@syntronic.com>
2023-11-26 21:07:35 -0500

    finished step 4.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
-
+import os
+from flask_sqlalchemy import SQLAlchemy  # new
 
 # instantiate the app
 app = Flask(__name__)
@@ -8,7 +9,24 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
+
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 class Ping(Resource):
     def get(self):
--- src/config.py
+++ src/config.py
@@ -1,17 +1,19 @@
 # src/config.py
-
+import os  # new
 
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SECRET_KEY = 'my_precious'  # new
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
